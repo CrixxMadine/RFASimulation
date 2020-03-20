1d7294f6248cf3396c74c79a75137299164d8d07
CrixxMadine <50016024+CrixxMadine@users.noreply.github.com>
2020-03-20 08:47:43 +0100

    [Create] SubFolder for MatLab files
    [add] PrototypeDGLSolver

---
+++ MatLab/PrototypeDGLSolver.m
@@ -0,0 +1,12 @@
+% This Skript is for experimenting with the RFA DGLs
+
+% for reference see article of my advisor:
+% Tim Kroeger et al. 
+% "Numerical Simulation of Radio Frequency
+%  Ablation with State Dependent Material
+%  Parameters in Three Space Dimensions"
+
+test = 0;
+disp(test);
+
+disp('MatLab ..... yeah :(')
