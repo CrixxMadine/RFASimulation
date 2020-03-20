beacbb66cf36124c1300596450759bf79ff773e3
CrixxMadine <50016024+CrixxMadine@users.noreply.github.com>
2020-03-20 10:56:16 +0100

    [mod] reforged project doc and MatLab
    Doc:
    - renamed files to english names
    - mod some content
    
    MatLab:
    - add values for material constants

--- MatLab/PrototypeDGLSolver.m
+++ MatLab/PrototypeDGLSolver.m
@@ -6,7 +6,16 @@
 %  Ablation with State Dependent Material
 %  Parameters in Three Space Dimensions"
 
-test = 0;
-disp(test);
-
 disp('MatLab ..... yeah :(')
+
+
+% Material parameters
+% For the first run, everything is const.
+
+% for ref. see books from Stein T. and Kröger T.
+sigma  = [ 0.21     0.013      -1     1.143 ]';
+rho    = [ 1080   -0.00056   -0.657     0   ]';
+c      = [ 3455       0      -0.376     0   ]';
+lambda = [ 0.437    0.0025      0       0   ]';
+
+
