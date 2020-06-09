55c2fb2901c8eaf00e15ae926baef4da6e350886
Martin Michel <martin.michel@mailbox.org>
2020-06-09 14:45:23 +0200

    Implemented AssembCylindricHeadEquation2D

--- MatLab/AssembCylindricHeatEquation2D.m
+++ MatLab/AssembCylindricHeatEquation2D.m
@@ -1,4 +1,4 @@
-function [Ah, Mh, fh] = AssemblCylindricHeatEquation2D(pmesh, tmesh, k, q, f_rhs, intyp)
+function [Ah, Mh, fh] = AssembCylindricHeatEquation2D(pmesh, tmesh, k, q, f_rhs, intyp)
 
 %% Function summary and arguments description
 
@@ -14,9 +14,47 @@ function [Ah, Mh, fh] = AssemblCylindricHeatEquation2D(pmesh, tmesh, k, q, f_rhs
 
 % -- Inout args are similar to AssembleCylindricLaPlace --
 
-Ah = 0;
-Mh = 0; 
-fh = 0;
+Ng = length(pmesh);    % total number of points
+Me = length(tmesh);    % total number of triangle elements
+Ne = 3;    % number of points per element - (3 for triangle)
+
+
+
+%% Assemble the system of equations, elementwise
+
+Ah = zeros(Ng,Ng);
+Mh = zeros(Ng,Ng);
+fh = zeros(Ng,1);
+
+for i=1:Me
+    
+    % get the global point numbers of the current triangle
+    a = [tmesh(1,i),  tmesh(2,i), tmesh(3,i)];
+    
+    % get the coordinates of each point
+    p1 = pmesh(:,a(1));
+    p2 = pmesh(:,a(2));
+    p3 = pmesh(:,a(3));
+    
+    % assemble matrix and right hand sand for the current element
+    [K_elem, f_elem, M_elem] = LinearElement2D(k, q, f_rhs, p1, p2, p3, intyp, 'true');
+    
+    % add element matrix and right hand side to the existing system
+    for m = 1:Ne  
+        
+       fh(a(m)) = fh(a(m)) + f_elem(m);  
+      
+       for n = 1:Ne
+           
+           Ah(a(m),a(n)) = Ah(a(m),a(n)) + K_elem(m,n);
+           
+           Mh(a(m),a(n)) = Mh(a(m),a(n)) + M_elem(m,n);
+       end 
+       
+    end 
+    
+end 
 
 end
+
 
