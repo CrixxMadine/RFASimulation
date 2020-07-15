9c0630828790aeb240d2411ba5330cea32cb256b
CrixxMadine <martin.michel@mailbox.org>
2020-07-15 23:57:43 +0200

    Add function shell for undefined boundary points

---
+++ MatLab/GetUndefinedBoundaryPoints.m
@@ -0,0 +1,9 @@
+function undefinedPoints = GetUndefinedBoundaryPoints(bmesh)
+
+% Problematic point = Neumann point next to neumann / dirichlet point
+% Return a vector with undefined points and its neighbours
+
+undefinedPoints;
+
+end
+
