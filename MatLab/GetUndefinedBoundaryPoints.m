d6d8c638986b8b246d2e838d9b2839e314d0ccc3
CrixxMadine <martin.michel@mailbox.org>
2020-07-18 13:24:19 +0200

    Added function description

--- MatLab/GetUndefinedBoundaryPoints.m
+++ MatLab/GetUndefinedBoundaryPoints.m
@@ -1,16 +1,39 @@
 function undefinedPoints = GetUndefinedBoundaryPoints(bmesh)
 
-hereIsDirich = find(bmesh(:,3) == 1);
-theseAreDirichNodes = unique([bmesh(hereIsDirich,1), bmesh(hereIsDirich,2)]);
+%% Function and parameter description
 
-hereIsNeumann = find(bmesh(:,3) == 2);
-neumannNodes = unique([bmesh(hereIsNeumann,1), bmesh(hereIsNeumann,2)]);
+% Find problematic boundary nodes in grid
+% 
+% Problematic node happens if there are different boundary conditions
+% colliding at one point e.g. the electrodes on the probe
+%
+% Since the dirichlet value has higher priority and therefore set the its value,
+% the next neighbour on the neumann or robin side is calculated wrong
+% 
+% This function is to detect these problematic nodes
+%
+% Input arg:
+% bmesh, where each row represents a boundary edge
+% First and second column are the bnodes, third column is boundary type
+% Boundary types: 1 -> Dirichlet, 2 -> Neumann, 3 -> Robin
+%
+% Returns:
+% undefinedPoints: -> n x 3 matrix with the problematic node information
+% First column contains the problematic point in pmesh
+% Second and third column are the neigbour nodes of the undefined node
 
-% Dirichlet corner nodes
-[problemCornerNodes] = intersect(theseAreDirichNodes, neumannNodes);
 
-numProblemPoints = size(problemCornerNodes,1);
+%% TODO clean up
+rowsDirichCondition = find(bmesh(:,3) == 1);
+allDirichNodes = unique([bmesh(rowsDirichCondition,1), bmesh(rowsDirichCondition,2)]);
 
+rowsNeumannCondition = find(bmesh(:,3) == 2);
+allNeumannNodes = unique([bmesh(rowsNeumannCondition,1), bmesh(rowsNeumannCondition,2)]);
+
+% Node with both edges with dirichlet condition and other condition
+[problematicNodes] = intersect(allDirichNodes, allNeumannNodes);
+
+numProblemPoints = size(problematicNodes,1);
 
 
 % Case no problematic points (e.g. only one type of boundary conditions)
@@ -21,15 +44,15 @@ end
 
 fuckYou(numProblemPoints) = 0;
 
-for count=1:length(problemCornerNodes)
-corner = problemCornerNodes(count);
+for count=1:length(problematicNodes)
+corner = problematicNodes(count);
 temp1 = find(bmesh(:,1)==corner);
 temp2 = find(bmesh(:,2)==corner);
 rowVonEcke = unique([temp1, temp2]);
 
 bla = [bmesh(rowVonEcke,1) bmesh(rowVonEcke,2)];
 
-yuhu = setdiff(bla, theseAreDirichNodes);
+yuhu = setdiff(bla, allDirichNodes);
 
 if (length(yuhu) > 0)
 fuckYou(count) = yuhu;
