ad1e964e3599196291998a8c2e2ad8c743f783fb
CrixxMadine <martin.michel@mailbox.org>
2020-07-19 00:05:54 +0200

    Cleaned up classes

--- MatLab/AssembCylindricHeatEquation2D.m
+++ MatLab/AssembCylindricHeatEquation2D.m
@@ -8,13 +8,14 @@ function [Ah, Mh, fh] = AssembCylindricHeatEquation2D(pmesh, tmesh, k, q, Q_val,
 % Assemble each element, no boundary conditions yet
 
 % returns:
-% Ah := sparse FE-matrix (sum of stiffnessmatrix and q-massmatrix)
-% Mh := sparse M-Matrix (the prefactor matrix of du/dt)
-% fh := right hand side of the system of equations
+%  Ah := sparse FE-matrix (sum of stiffnessmatrix and q-massmatrix)
+%  Mh := sparse M-Matrix (the prefactor matrix of du/dt)
+%  fh := right hand side of the system of equations
 
-% -- Inout args are very similar to AssembleCylindricLaPlace --
-% New arg: Q_val -> replacement for f_rhs
-% Discrete vlaue for the right hand side
+% -- Input args are very similar to AssembleCylindricLaPlace --
+%  New arg: Q_val -> replacement for f_rhs
+%  Discrete value for the right hand side
+
 
 %% Define additional parameters
 
@@ -37,9 +38,6 @@ fh = zeros(Ng,1);
 
 count = 0;
 
-% OLD -> Using full matrix
-% Ah = zeros(Ng,Ng);
-% Mh = zeros(Ng,Ng);
 
 for i=1:Me
     
@@ -53,13 +51,8 @@ for i=1:Me
     
     % Define f_rhs as constant average value from the triangulation
     f_rhs = @(r,z) 1 / 3 * (Q_val(a(1)) + Q_val(a(2)) + Q_val(a(3)));
-    
-    % TODO DELETE
-    %Q_perf_val = 1 / 3 * Q_perf(a(1)) + Q_perf(a(2)) + Q_perf(a(3));
-    % Better use pdegrad for triangle
-    %f_rhs = @(r,z) Q_rfa(i) + Q_perf_val;
-    
-    % assemble matrix and right hand sand for the current element
+        
+    % Assemble matrix and right hand sand for the current element
     [K_elem, f_elem, M_elem] = LinearElement2D(k, q, f_rhs, p1, p2, p3, intyp, 'true');
     
     % add element matrix and right hand side to the existing system
@@ -78,11 +71,7 @@ for i=1:Me
            Mh_row(count) = a(m);
            Mh_col(count) = a(n);
            Mh_val(count) = M_elem(m,n); 
-           
-           % OLD -> Using full matrix
-           % Ah(a(m),a(n)) = Ah(a(m),a(n)) + K_elem(m,n);          
-           % Mh(a(m),a(n)) = Mh(a(m),a(n)) + M_elem(m,n);
-           
+                      
        end 
        
     end 
--- MatLab/AssembCylindricLaplace2D.m
+++ MatLab/AssembCylindricLaplace2D.m
@@ -7,46 +7,48 @@ function [Ah, fh] = AssembCylindricLaplace2D(pmesh, tmesh, k, q, f_rhs, intyp)
 % Model equation: - div( k * grad(u) ) + q * u = f_rhs
 
 % returns:
-% Ah := sparse FE-matrix (sum of stiffnessmatrix and q-massmatrix)
-% fh := right hand side of the system of equations
+%  Ah := sparse FE-matrix (sum of stiffnessmatrix and q-massmatrix)
+%  fh := right hand side of the system of equations
 
 % Input args:
-% pmesh := point matrix of triangulation
-% tmesh := triangle matrix of triangulation
-% k, q  := coeffiients of the pde
-% f_rhs := right hand side of the pde
+%  pmesh := point matrix of triangulation
+%  tmesh := triangle matrix of triangulation
+%  k, q  := coeffiients of the pde
+%  f_rhs := right hand side of the pde
 %
-% eltyp := type of regression function
-% -> 1 = linear triangle element
+%  eltyp := type of regression function
+%  -> 1 = linear triangle element
 %
-% intyp := type of numerical quadrature
-% -> 0 = with matlab library function
-% -> 1 = own implementation of gauss quadrature
+%  intyp := type of numerical quadrature
+%  -> 0 = with matlab library function
+%  -> 1 = own implementation of gauss quadrature
 
 
-% explanations on the grid args (pmesh, emesh, tmesh):
+%  explanations on the grid args (pmesh, emesh, tmesh):
 %
-% pmesh:  point matrix 
-% Contains all the r and z coordinates of the points
+%  pmesh:  point matrix 
+%  Contains all the r and z coordinates of the points
 %
-% tmesh:  triangle matrix 
-% First three lines: corners of the trinagle, counted counterclockwise 
-% Fourth line: subdomain number (to be reserved;
-%              not used by now, but could be used for material properties)
+%  tmesh:  triangle matrix 
+%  First three lines: corners of the trinagle, counted counterclockwise 
+%  Fourth line: subdomain number (to be reserved;
+%               not used by now, but could be used for material properties)
 %
-% bmesh:  boundray edge matrix : information on edges and its types 
+%  bmesh:  boundray edge matrix : information on edges and its types 
 %         -> to be used for boundary conditions
 
 
-%% Define helper parameters
+%% Implementation
+
+
+% Define helper parameters
 
 Ng = length(pmesh);    % total number of points
 Me = length(tmesh);    % total number of triangle elements
 Ne = 3;    % number of points per element - (3 for triangle)
 
 
-
-%% Assemble the system of equations, elementwise
+% Assemble the system of equations, elementwise
 
 Ah_row = zeros(3*Ng,1);
 Ah_col = Ah_row;
@@ -55,23 +57,20 @@ Ah_val = Ah_row;
 fh = zeros(Ng,1);
 count = 0;
 
-% OLD -> Using no sparse matrix
-% Ah = zeros(Ng,Ng);
-
 for i=1:Me
     
-    % get the global point numbers of the current triangle
+    % Get the global point numbers of the current triangle
     a = [tmesh(i,1),  tmesh(i,2), tmesh(i,3)];
     
-    % get the coordinates of each point
+    % Get the coordinates of each point
     p1 = pmesh(a(1),:)';
     p2 = pmesh(a(2),:)';
     p3 = pmesh(a(3),:)';
     
-    % assemble matrix and right hand sand for the current element
+    % Assemble matrix and right hand sand for the current element
     [K_elem, f_elem] = LinearElement2D(k, q, f_rhs, p1, p2, p3, intyp);
     
-    % add element matrix and right hand side to the existing system
+    % Add element matrix and right hand side to the existing system
     for m = 1:Ne  
         
        fh(a(m)) = fh(a(m)) + f_elem(m);  
@@ -84,8 +83,6 @@ for i=1:Me
            Ah_col(count) = a(n);
            Ah_val(count) = K_elem(m,n); 
            
-           % OLD -> Using no sparse matrix
-           % Ah(a(m),a(n)) = Ah(a(m),a(n)) + K_elem(m,n);
        end 
        
     end 
