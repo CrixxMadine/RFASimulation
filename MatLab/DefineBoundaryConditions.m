a86a20c7e78c554e2181cf89f002a84ef1d3b365
CrixxMadine <martin.michel@mailbox.org>
2020-07-19 00:13:39 +0200

    Cleaned up class

--- MatLab/DefineBoundaryConditions.m
+++ MatLab/DefineBoundaryConditions.m
@@ -1,27 +1,30 @@
 function bmesh = DefineBoundaryConditions(bedges, type)
 
+%% Function and parameter description
+
 % Define the boundary conditions for the current mesh grid
-% 
+% Adds third and fourth row with info on bmesh
+ 
 % returns
-% bmesh -> third row is type (1=Dirichlet, 2=Neumann)
-%       -> fourth row is corresponding value
-% 
+%  bmesh -> third row is type (1=Dirichlet, 2=Neumann)
+%        -> fourth row is corresponding value
+ 
 % input args:
-% type: the type of the boundary equation to be added
-% -> 'phi'  : add conditions for electrode 
-% -> 'temp' : add boundary conditions for heat equation
-% -> more will be added within time!
+%  type: the type of the boundary equation to be added
+%  -> 'phi'  : add conditions for electrode 
+%  -> 'temp' : add boundary conditions for heat equation
 
 % bedges: 
-% Third row in bedges explained:
-% Every boundary domain is represented by a number
+%  Third row in bedges explained:
+%  Every boundary domain is represented by a number
 %    domain1   -> '100' : positive electrode
 %    domain2   -> '101' : negative electrode
 %    domain3   -> '200' : blank needle
 %    domain4   -> '300' : outer boundary
 %    domain5   -> '400' : rotation axis
-% 
-%  TODO : will define robin and needle in time 
+
+
+%% Implementation
 
 n = size(bedges, 1);
 
@@ -41,43 +44,46 @@ for i=1:n
             bmesh(i,3) =  1;
             bmesh(i,4) = -1;
 
+        % Optional -> Simulate mass to nullify potential   
+       % elseif (bedges(i,3) == 300) % Outer boundary              
+       %      bmesh(3,i) = 1;        % Dirich
+       %      bmesh(4,i) = 0;
+        
         else % blank needle or outer boundary or rotation axis
             bmesh(i,3) = 2;     % Neumann 
             bmesh(i,4) = 0;
             
-            % Testing
-%             bmesh(3,i) = 1;     % Dirich
-%             bmesh(4,i) = 0;
+
         end
     
     elseif strcmp(type, 'temp')
         
-        %if (1 < 2)  %% TODO TESTING
         if (bedges(i,3) == 300) % outer boundary
             bmesh(i,3) = 2;     % Neumann 
             bmesh(i,4) = 0;
          
-        %elseif(2<1)
         elseif (bedges(i,3) == 400) % rotation axis
-            bmesh(i,3) = 2;     % Neumann 
+            bmesh(i,3) = 2;         % Neumann 
             bmesh(i,4) = 0;
          
-            % Testing
-        else
-            bmesh(i,3) = 2;     % Neumann 
+       % Variant one, probe is not cooled    
+        else                   % anywhere else
+            bmesh(i,3) = 2;    % Neumann 
             bmesh(i,4) = 0;            
-            
-        %else     % anywhere on the needle, blank or electrode
-         %   bmesh(i,3) = 1;           % Dirichlet
-         %   bmesh(i,4) = 37 + 273.15; % body temperature in Kelvin
+       
+       % Variant two, cooled probe
+       % else     % anywhere on the needle, blank or electrode
+       %     bmesh(i,3) = 1;           % Dirichlet
+       %     bmesh(i,4) = 37 + 273.15; % body temperature in Kelvin
         end
         
     else   
         
-        error('You did not specify an allowed equation type!')
+        error('You did not specify an allowed type! Use phi or temp')
         
-    end % if type
+    end 
      
-end % for
+end 
+
 end
 
