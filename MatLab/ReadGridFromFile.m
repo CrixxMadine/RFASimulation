8286d3ceea11a00215e97fc04752db71d63390c3
CrixxMadine <martin.michel@mailbox.org>
2020-07-19 00:15:32 +0200

    Cleaned up class

--- MatLab/ReadGridFromFile.m
+++ MatLab/ReadGridFromFile.m
@@ -1,22 +1,19 @@
 function [pmesh, tmesh, bedges] = ReadGridFromFile(folderPath)
 
-% Reads a grid from given text file
-% Expects a folder path containing text files named:
-% pmesh.txt, tmesh.txt, bedges.txt, binfo.txt, bconversion.txt
+%  Reads a grid from given text file
+%  Expects a folder path containing text files named:
+%  pmesh.txt, tmesh.txt, bedges.txt, binfo.txt, bconversion.txt
 %
-% returns pmesh, tmesh, bedges 
+%  returns pmesh, tmesh, bedges 
 % 
-% folderPath := location of the folder containing the upper text files
+%  folderPath := location of the folder containing the upper text files
 %
-% Will do more generic file input as soon as needed
-% For testing purposes this is enough
-%
-% This is how the files must be formatted: 
-% pmesh.txt  : x and y coordinates of the points
-% tmesh.txt  : number of p1, p2, and p3 of the triangle
-% bedges.txt : number of p1 and p2 of the boundary edge
-% binfo.txt  : number of boundary or the boundary in bedges
-% bconversion.txt : describes which boundary number represents what domain
+%  This is how the files must be formatted: 
+%  pmesh.txt  : x and y coordinates of the points
+%  tmesh.txt  : number of p1, p2, and p3 of the triangle
+%  bedges.txt : number of p1 and p2 of the boundary edge
+%  binfo.txt  : number of boundary or the boundary in bedges
+%  bconversion.txt : describes which boundary number represents what domain
 %    domain1   -> '100' : positive electrode
 %    domain2   -> '101' : negative electrode
 %    domain3   -> '200' : blank needle
@@ -28,8 +25,8 @@ pmesh = load(strcat(folderPath, 'pmesh.txt'));
 
 tmesh = load(strcat(folderPath, 'tmesh.txt'));
 
-% MatLab counts indizes from one
-% Grid Data counts indizes from zero
+% MatLab counts indizes from one -> 
+% Mesh data on files counts indizes from zero
 tmesh  = ShiftAllPointsByOne(tmesh);
 
 
@@ -49,19 +46,26 @@ btype = ConvertBoundaryType(bnumbers, bconverter(:,2))';
 
 bedges = [bpoints btype]; 
 
-%% Helper functions
+
+%% Mini helper functions
 
 function matrix = ShiftAllPointsByOne(matrix)        
+    
     matrix = matrix + 1;
+    
 end
 
+
 function newInfo = ConvertBoundaryType(boundaries, conversion)
+    
     n = size(boundaries);
     newInfo(n) = 0;
 
     for i=1:n
+        
         number = boundaries(i);
         newInfo(i) = conversion(number); 
+        
     end
 
 end
